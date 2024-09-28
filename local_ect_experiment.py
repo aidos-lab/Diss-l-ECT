3701bcc608b47c55e0b0fc422dec6920dc1af681
julius-vr <44684046+julius-vr@users.noreply.github.com>
2024-09-28 13:39:12 +0200

    Update local_ect_experiment.py

--- local_ect_experiment.py
+++ local_ect_experiment.py
@@ -1,21 +1,6 @@
 from local_ect import *
 
 def main():
-    # Range over dataset, radius
-
-    # dataset = WikiCS(root='/tmp/wikics')
-
-    # For the heterophilous datasets use ROC AUC!
-    # dataset = HeterophilousGraphDataset(root='/tmp/Minesweeper',name='Minesweeper')
-    # dataset = HeterophilousGraphDataset(root='/tmp/Tolokers',name='Tolokers')
-    # dataset = HeterophilousGraphDataset(root='/tmp/Questions',name='Questions')
-
-    # dataset = Actor(root='/tmp/actor')
-
-    # Reddit is hard to compute!!
-    # dataset = Reddit(root='/tmp/reddit')
-
-    # dataset = Coauthor(root='/tmp/CS',name='CS')
     dataset = Coauthor(root='/tmp/Physics', name='Physics')
 
     res = xgb_model(dataset,
